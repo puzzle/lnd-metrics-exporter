18ce73da02b83b83b85e0657ec8087c8799fad8c
Adrian Pauli <pauli.adrian96@gmail.com>
2019-06-08 13:22:39 +0200

    Add basic stuff

---
+++ src/main/java/ch/puzzle/lnd/metricsexporter/MetricsExporterApplication.java
@@ -0,0 +1,13 @@
+package ch.puzzle.lnd.metricsexporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetricsExporterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetricsExporterApplication.class, args);
+	}
+
+}
